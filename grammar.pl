34b36a3b5b0abeb419af842b2a89a656eca8fd93
Austin Derbique <aderbique@aggiemail.usu.edu>
2017-10-25 06:04:57 +0000

    progress

---
+++ grammar.pl
@@ -0,0 +1,96 @@
+agricultural_science
+animal_science
+avenue
+bedroom
+bedroom_closet
+chemistry_lab
+closet
+common_room
+computer_lab
+elevator
+engr
+eslc_north
+eslc_south
+geology_building
+hall
+hub
+kitchen
+laser_lab
+library
+observatory
+old_main
+plaza
+quad
+roof
+roommate_room
+secret_lab
+ser_1st_floor
+ser_2nd_floor
+special_collections
+tsc
+tsc_patio
+tunnels_east
+tunnels_north
+tunnels_west
+
+
+go_verb(move) --> [climb].
+go_verb(move) --> [go].
+go_verb(move) --> [head].
+go_verb(move) --> [move].
+go_verb(move) --> [run].
+go_verb(move) --> [travel].
+go_verb(move) --> [walk].
+go_verb(move) --> [skip].
+
+
+here_verb(take) --> [take].
+here_verb(take) --> [steal].
+here_verb(take) --> [pick].
+here_verb(take) --> [grab].
+here_verb(put) --> [place].
+here_verb(put) --> [drop].
+here_verb(put) --> [put].
+
+other_verb(look) --> [look].
+other_verb(inventory) --> [inventory].
+other_verb(inventory) --> [i].
+other_verb(stop) --> [exit].
+other_verb(stop) --> [quit].
+other_verb(stop) --> [kill].
+other_verb(stop) --> [halt].
+
+
+a
+an
+around
+in
+inside
+the
+to
+toward
+up
+
+
+bone
+book_a
+book_b
+book_c
+bunsen_burner
+charged_bone
+clean_clothes
+coat
+dirty_clothes
+figurine
+flask
+fly
+goggles
+green_beam
+ice_cream
+key
+laser
+lost_homework
+movie
+note
+potion
+recipe
